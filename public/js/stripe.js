646177bc3d9cf98705269e91440f095b26d07734
simonet85 <christian.85@live.fr>
2021-05-24 23:09:49 +0200

    initial commit

---
+++ public/js/stripe.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe('pk_test_51HUyKGCM2FrEvLYrdv9Yxx1N9rbkzJpT0VLOCggyDlYiaLRxSLhQ2vbBcmKlpczbsbgvSUBY2iseWeYx9fBR129A00zWn3o9IO');
+
+export const bookTour = async tourId => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    // console.log(session);
+
+    // 2) Create checkout form + chanre credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
